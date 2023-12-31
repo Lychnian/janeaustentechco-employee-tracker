d75eec0997c1965f24c005ca7400375986dce484
unknown <helenvcolon@gmail.com>
2023-12-31 15:47:57 -0500

    Added function to start application and display options in server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 const mysql = require('mysql2/promise'); // Using promise-based mysql2
 const cfonts = require('cfonts');
 
-
 // Async function to initialize the database connection
 async function initializeDatabase() {
     try {
@@ -17,5 +16,59 @@ async function initializeDatabase() {
     } catch (err) {
         console.error('Error connecting to the database:', err);
         process.exit(1);
+    }
+}
+
+// Function to start the application and display options
+async function start() {
+    try {
+        const answer = await inquirer.prompt({
+            type: "list",
+            name: "action",
+            message: "Welcome to the Jane Austen Tech Co Employee Tracker. What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "EXIT"
+            ]
+        });
+
+        switch (answer.action) {
+            case "View all departments":
+                await viewAllDepartments();
+                break;
+            case "View all roles":
+                await viewAllRoles();
+                break;
+            case "View all employees":
+                await viewAllEmployees();
+                break;
+            case "Add a department":
+                await addDepartment();
+                break;
+            case "Add a role":
+                await addRole();
+                break;
+            case "Add an employee":
+                await addEmployee();
+                break;
+            case "Update an employee role":
+                await updateEmployeeRole();
+                break;
+            case "Exit":
+                console.log("Goodbye! Have a nice day!");
+                connection.end();
+                return;
+        }
+
+        await start();
+    } catch (err) {
+        console.error('Error during operation:', err);
+        await start();
     }
 }
