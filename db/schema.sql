9bee50ad38b728f3d430a2c4102f81ef95048e80
unknown <helenvcolon@gmail.com>
2023-12-31 02:40:45 -0500

    Created db folder, schema file (first lines of code), and seeds file (added departments)

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drop the existing database if it exists. This is useful for resetting the database.
+DROP DATABASE IF EXISTS janeausten_techco_employee_db;
+
+-- Create a new database for the application.
+CREATE DATABASE janeausten_techco_employee_db;
+
+-- Select the newly created database for subsequent operations.
+USE janeausten_techco_employee_db;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- Inserting data into the 'departments' table
+-- This will create four departments: Executive, Engineering, Development, and Machine Learning.
+INSERT INTO departments (department_name)
+VALUES
+    ("Executive"),              -- CEO Jane Austen's department
+    ("Engineering"),            -- Engineering department
+    ("Development"),            -- Development department
+    ("Machine Learning");       -- Machine Learning department
